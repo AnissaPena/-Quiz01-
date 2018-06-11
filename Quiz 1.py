9c4b3fc883df472d33b0d8cba32cbee3c3fdacbd
AnissaPena <40004741+AnissaPena@users.noreply.github.com>
2018-06-11 16:03:40 -0500

    Update Quiz 1.py
    Updated and finalized

--- Quiz 1.py
+++ Quiz 1.py
@@ -3,35 +3,30 @@ def matVec(matrix, vector):
     '''
 This function takes a matrix and a vector as it's arguments. It then updates each element of the vector by multiplying it by the matrix before returning the now updated vector. 
     '''
-def matvec(matrix, vector):
-    
+    result = []
     for i in range(len(matrix)):
-        for j in range(len(vector)):
-            result = matrix[i][j] * vector[j]
-        result.append(total)
-        
+      total=0
+      for j in range(len(vector)):
+        total += matrix[i][j] * vector[j]
+      result.append(total)
     return result
+
+# in the above code I am designating total as 0 so that when the computation of matrix and vector it will be added to the result that the matrix and vector multiplication creates. 
    
 #Below are test variables being used to compute the function matVec
  
- def matvec(matrix, vector):
-     matrix = A
-     vector = x
- # 2x2 matrices   
+# 2x2 matrices   
      
-
-testmatrix01 = [2,3],[1,4]
-testmatrix02 = [1,3],[5,0]
+testmatrix01 = [[2,1],[1,4]]
+testvector02 = [[1,3],[5,0]]
 testmatrix03 = 'this is a test'
 
-testVector01 = [1, 2]
-testVector02 = [2,4]
+testVector01 = [1, 1]
+testmatrix02 = [2,4]
 testVector03 = True
 
 # These are test cases for the function matVec. All but one of the tests should be commented out at a time so that we can see how each pair of inputs effects the output. 
 
 print(matVec(testmatrix01,testVector01))
 #print(matVec(testmatrix02,testVector02))
-#print(matVec(testmatrix03,testVector03))
-
-
+#print(matVec(testmartrix03,testVector02))
